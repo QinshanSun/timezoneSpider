3865978d44e90152369af30c35521ca9893b7174
I317663 <qinshan.sun@sap.com>
2019-01-21 18:36:02 +0800

    Get Country Info Json
    Get Country Info Json

--- spider/Model.py
+++ spider/Model.py
@@ -1,44 +1,127 @@
 # -*- coding: utf-8 -*-
 
+import json
+import re
+
 
 class TimeZone(object):
+    __attrs__ = ['description', 'country', 'code', 'name', 'link', 'utc_timezone']
 
-    def __init__(self, code, name):
-        self.code = code
-        self.name = name
-
-    def set_description(self, description):
-        setattr(self, "description", description)
-
-    def get_description(self):
-        getattr(self, "description")
-
-    def set_country(self, country):
-        setattr(self, "country", country)
-
-    def get_country(self):
-        getattr(self, "country")
+    def __init__(self):
+        self.code = None
+        self.name = None
+        self.description = None
+        self.country = []
+        self.link = None
+        self.utc_timezone = None
 
 
-class Country(object):
+class Entity(object):
+    __attrs__ = ['en_name', 'name', 'link']
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = None
+        self.en_name = None
+        self.link = None
 
-    def get_en_name(self):
-        getattr(self, 'en_name')
+    def get_en_name_for_entity(self, soup):
+        en_name_soup = soup.find('h2')
+        p1 = re.compile(r'[(](.*?)[)]', re.S)
+        re.findall(p1, en_name_soup.text.strip())
+        print(en_name_soup.text.strip())
+        print(re.findall(p1, en_name_soup.text.strip()))
+        en_name_list = re.findall(p1, en_name_soup.text.strip())
+        if len(en_name_list) > 0:
+            self.en_name = en_name_list[0]
 
-    def set_en_name(self, name):
-        setattr(self, 'en_name', name)
+
+class Country(Entity):
+    __attrs__ = ['en_name', 'name', 'city', 'link', 'summer_time', 'is_summer_time', 'utc_timezone']
+
+    def __init__(self):
+        super().__init__()
+        self.city = []
+        self.is_summer_time = None
+        self.utc_timezone = None
+        self.summer_time = None
+
+    def get_city_info_for_country(self, soup):
+        city_list = []
+        city_time_zone_td = soup.find('td', class_='col-c1')
+        if city_time_zone_td is not None and len(city_time_zone_td) > 0:
+            city_link_list = city_time_zone_td.find_all('a')
+            for city_link in city_link_list:
+                city = City()
+                city.name = city_link.text
+                city.link = city_link['href']
+                city_list.append(city)
+                # print(json.dumps(city.__dict__, ensure_ascii=False, cls=ObjectEncoder))
+        self.city = city_list
+        print(json.dumps(self.__dict__, ensure_ascii=False, cls=ObjectEncoder))
+        # country_list.append(json.dumps(country.__dict__, ensure_ascii=False, cls=ObjectEncoder))
+
+    def get_detailed_info_for_country(self, soup):
+        data_block_soup = soup.find('div', class_='dataBlock dBColor3')
+        city_detailed_info_list = data_block_soup.find_all('div', class_='infoRow')
+        city_detailed_info_title_list = data_block_soup.find_all('h3', class_='infoRowTitle')
+        if len(city_detailed_info_list) > 0 and len(city_detailed_info_title_list) > 0:
+            for index in range(len(city_detailed_info_title_list)):
+                if 'GMT' in city_detailed_info_title_list[index].text:
+                    # print(city_detailed_info_list[index].text)
+                    # print(city_detailed_info_list[index].text.strip().replace(" ", ""))
+                    city_offset_time_td = city_detailed_info_list[index].find_all('td')
+                    if len(city_offset_time_td) > 0:
+                        self.utc_timezone = city_offset_time_td[0].text.strip().replace(" ", "")
+                    if len(city_offset_time_td) > 1:
+                        self.is_summer_time = city_offset_time_td[1].text.strip().replace(" ", "")
+
+                if '夏令时' in city_detailed_info_title_list[index].text:
+                    print(city_detailed_info_list[index].text.strip().replace(" ", ""))
+                    self.summer_time = city_detailed_info_list[index].text.strip().replace(" ", "")
 
 
-class City(object):
+class City(Entity):
+    __attrs__ = ['en_name', 'name', 'link', 'summer_time', 'longitude', 'latitude', 'is_summer_time', 'utc_timezone']
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        super().__init__()
+        self.summer_time = None
+        self.longitude = None
+        self.latitude = None
+        self.is_summer_time = None
+        self.utc_timezone = None
 
-    def get_en_name(self):
-        getattr(self, 'en_name')
+    def get_detailed_info_for_city(self, soup):
+        data_block_soup = soup.find('div', class_='dataBlock dBColor3')
+        city_detailed_info_list = data_block_soup.find_all('div', class_='infoRow')
+        city_detailed_info_title_list = data_block_soup.find_all('h3', class_='infoRowTitle')
+        if len(city_detailed_info_list) > 0 and len(city_detailed_info_title_list) > 0:
+            for index in range(len(city_detailed_info_title_list)):
+                if 'GMT' in city_detailed_info_title_list[index].text:
+                    # print(city_detailed_info_list[index].text)
+                    # print(city_detailed_info_list[index].text.strip().replace(" ", ""))
+                    city_offset_time_td = city_detailed_info_list[index].find_all('td')
+                    if len(city_offset_time_td) > 0:
+                        self.utc_timezone = city_offset_time_td[0].text.strip().replace(" ", "")
+                    if len(city_offset_time_td) > 1:
+                        self.is_summer_time = city_offset_time_td[1].text.strip().replace(" ", "")
 
-    def set_en_name(self, name):
-        setattr(self, 'en_name', name)
+                if '夏令时' in city_detailed_info_title_list[index].text:
+                    print(city_detailed_info_list[index].text.strip().replace(" ", ""))
+                    self.summer_time = city_detailed_info_list[index].text.strip().replace(" ", "")
+
+                if '地理坐标' in city_detailed_info_title_list[index].text:
+                    # print(city_detailed_info_list[index].text.strip().replace(" ", ""))
+                    city_location_td_list = city_detailed_info_list[index].find_all('td')
+                    if len(city_location_td_list) > 0:
+                        self.latitude = city_location_td_list[0].text.strip().replace(" ", "")
+                    if len(city_location_td_list) > 1:
+                        self.longitude = city_location_td_list[1].text.strip().replace(" ", "")
+
+
+class ObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (TimeZone, City, Country)):
+            return json.dumps(obj.__dict__, ensure_ascii=False, default=lambda o: o.__dict__,
+                              sort_keys=True)
+        return json.JSONEncoder.default(self, obj)
--- spider/Spider.py
+++ spider/Spider.py
@@ -1,9 +1,113 @@
 # -*- coding: utf-8 -*-
+import json
+import logging
 import os
+
 import requests
+import re
+from bs4 import BeautifulSoup
+import configparser
+
+from spider.Model import TimeZone, ObjectEncoder, City, Country
+
+logging.basicConfig(level=logging.INFO, filename='output.log',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-r = requests.get('https://24timezones.com/shi_jie3.php', headers=headers)
+basic_url = u'https://24timezones.com'
+
+
+class Spider(object):
+
+    def __init__(self):
+        self.base_url = u'https://24timezones.com'
+
+    def get_time_zone_basic_info(self):
+        soup = self.get_response_from_url(basic_url + '/shi_jie3.php')
+        time_zone_table = soup.find('table', class_='dataTab1 genericBlock')
+        time_zone_table_rows = time_zone_table.find_all('tr')
+        time_zones = []
+        for time_zone_table_row in time_zone_table_rows:
+            time_zone_table_data = time_zone_table_row.find_all('td')
+            if len(time_zone_table_data) > 0:
+                time_zone = TimeZone()
+                time_zone.code = time_zone_table_data[0].text.strip()
+                time_zone.link = time_zone_table_data[0].a['href']
+                time_zone.name = time_zone_table_data[1].text.strip()
+                # print(time_zone.name)
+                # print(json.dumps(time_zone.__dict__, ensure_ascii=False))
+                time_zone = self.get_time_zone_detailed_info(time_zone)
+                logging.info(json.dumps(time_zone.__dict__, ensure_ascii=False))
+                time_zones.append(time_zone)
+        print(json.dumps(time_zones, ensure_ascii=False, cls=ObjectEncoder))
+
+    def get_time_zone_detailed_info(self, time_zone):
+        time_zone_detailed_info_url = time_zone.link
+        soup = self.get_response_from_url(basic_url + time_zone_detailed_info_url)
+        time_zone_detailed_info_soup = soup.find('div', class_='dataBlock dBColor3')
+        time_zone_detailed_info = time_zone_detailed_info_soup.find_all('div', class_='infoRow')
+        time_zone_detailed_info_title = time_zone_detailed_info_soup.find_all('h3', class_='infoRowTitle')
+        # print(time_zone_detailed_info)
+        if len(time_zone_detailed_info) > 2 and len(time_zone_detailed_info_title) > 2:
+            if time_zone.code + " " in time_zone_detailed_info_title[0].text:
+                time_zone.description = time_zone_detailed_info[0].text.strip()
+            if 'GMT' in time_zone_detailed_info_title[1].text:
+                time_zone.offset_time = time_zone_detailed_info[1].text.strip().replace(" ", "")
+        return time_zone
+
+    def get_country_info(self):
+        soup = self.get_response_from_url(basic_url + '/shi_jie2.php')
+        country_time_zone_tables = soup.find_all('table', class_='dataTab1 genericBlock')
+        country_list = []
+
+        if len(country_time_zone_tables) == 2:
+            for country_time_zone_table in country_time_zone_tables:
+
+                country_time_zone_tr_list = country_time_zone_table.find_all('tr')
+                for country_time_zone_tr in country_time_zone_tr_list:
+                    country_link = country_time_zone_tr.find('a', class_='country_link')
+                    if country_link is not None:
+                        country = Country()
+                        country.name = country_link.text
+                        country.link = country_link['href']
+                        country_soup = self.get_response_from_url(basic_url + country.link)
+                        country.get_en_name_for_entity(country_soup)
+                        # print(json.dumps(country.__dict__, ensure_ascii=False, cls=ObjectEncoder))
+                        country.get_city_info_for_country(country_time_zone_tr.find_next_sibling())
+                        country.get_detailed_info_for_country(country_soup)
+                        if len(country.city) > 0:
+                            for city in country.city:
+                                city_soup = spider.get_response_from_url(basic_url + city.link)
+                                city.get_en_name_for_entity(city_soup)
+                                city.get_detailed_info_for_city(city_soup)
+                        country_list.append(country)
+
+        print(json.dumps(country_list, ensure_ascii=False, cls=ObjectEncoder))
+        # print(json.dumps(country_list, ensure_ascii=False, cls=ObjectEncoder))
+
+    @staticmethod
+    def get_response_from_url(url):
+        response = requests.get(url, headers=headers)
+        if response.status_code == requests.codes.ok:
+            soup = BeautifulSoup(response.content, "html.parser")
+            return soup
+        else:
+            print(response.status_code)
+
+
+spider = Spider()
+# spider.get_time_zone_basic_info()
+# spider.get_country_info()
+# city = City()
+spider.get_time_zone_basic_info()
+# city.link = '/zh_shi/tirana_shi_zhong.php'
+# city.name = u"地拉那"
+
+# city_soup = spider.get_response_from_url(basic_url + city.link)
+# city.get_en_name_for_entity(city_soup)
+# city.get_detailed_info_for_city(city_soup)
+# print(json.dumps(city, ensure_ascii=False, cls=ObjectEncoder))
